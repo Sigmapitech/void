40d97acb87c49ed57780d5789faccd4c1823663f
Renaud Manet <renaud.manet@epitech.eu>
2025-11-16 03:32:08 +0100

    bootstrap: add Part2-SExpr-Ex1

--- lisp/Main.hs
+++ lisp/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import System.Exit (exitSuccess)
+-- S-Expression definition
+data SExpr
+  = SInteger Integer
+  | SSymbol String
+  | SList [SExpr]
+  deriving Show
 
 main :: IO ()
 main =
