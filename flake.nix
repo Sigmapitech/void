5196c2d72a5c56ca618066156d4152093bd6ce72
Sigmanificient <edhyjox@gmail.com>
2025-11-17 16:02:12 +0100

    global: setup formatting git hooks

--- flake.nix
+++ flake.nix
@@ -1,10 +1,32 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, git-hooks }: let
     applySystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
     eachSystem = f: applySystems (system: f nixpkgs.legacyPackages.${system});
+
+    inherit (nixpkgs) lib;
   in {
+    checks = eachSystem (
+      pkgs: {
+        pre-commit-check = git-hooks.lib.${pkgs.system}.run {
+          src = ./.;
+          hooks = lib.genAttrs [
+            "alejandra"
+            "cabal-fmt"
+            "ormolu"
+            "hlint"
+          ] (x: { enable = true; });
+        };
+      }
+    );
+
     devShells = eachSystem (pkgs: let
       haskell = pkgs.haskell.packages.ghc984;
     in {
