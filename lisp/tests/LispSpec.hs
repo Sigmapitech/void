ab876e58910ee0c423887b8778f2086503eda7be
Renaud Manet <renaud.manet@epitech.eu>
2025-12-01 13:27:24 +0100

    lisp: add hspec unit tests for the bootstrap module

--- lisp/tests/LispSpec.hs
+++ lisp/tests/LispSpec.hs
@@ -1,13 +1,8 @@
 module Main (main) where
 
+import LispBootstrapSpec (bootstrapModuleSpec)
 import Test.Hspec
-
-dummySpec :: Spec
-dummySpec = do
-  describe "dummy test" $ do
-    it "Should always pass" $ do
-      True `shouldBe` True
 
 main :: IO ()
 main = hspec $ do
-  dummySpec
+  bootstrapModuleSpec
