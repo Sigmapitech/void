0a69cae514d31f51d7a12f63d08c0c5f6924efd6
Evan Mahe <evan.mahe@epitech.eu>
2025-12-01 10:32:33 +0100

    lisp: add units tests for the sexpr to ast

--- lisp/tests/LispSpec.hs
+++ lisp/tests/LispSpec.hs
@@ -2,9 +2,11 @@ module Main (main) where
 
 import LispAstSpec (astModuleSpec)
 import LispBootstrapSpec (bootstrapModuleSpec)
+import SexprtoASTSpec
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   bootstrapModuleSpec
   astModuleSpec
+  SexprtoASTSpec.spec
