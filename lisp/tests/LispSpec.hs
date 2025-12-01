d3eb02c5c3e48537bb2b3b23613d4929a6bee815
Sigmanificient <edhyjox@gmail.com>
2025-12-01 10:57:37 +0100

    lisp: apply hunks from duplicated commits
    Co-authored-by: Fenriir42 <raid3r2205@gmail.com>

--- lisp/tests/LispSpec.hs
+++ lisp/tests/LispSpec.hs
@@ -7,19 +7,15 @@
 
 module Main (main) where
 
-import ComplexExamplesSpec (spec)
-import EvaluatorSpec (spec)
 import LispAstSpec (astModuleSpec)
 import LispBootstrapSpec (bootstrapModuleSpec)
-import SexprtoASTSpec (spec)
-import SpecParser (parserSpec)
-import Test.Hspec (hspec)
+import SexprtoASTSpec
+import SpecParser
+import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  parserSpec
   bootstrapModuleSpec
   astModuleSpec
-  EvaluatorSpec.spec
-  ComplexExamplesSpec.spec
   SexprtoASTSpec.spec
+  parserSpec
