d7b78d4740d746bed7de09f24bc64361fc3cf220
Evan Mahe <evan.mahe@epitech.eu>
2025-12-05 15:34:04 +0100

    lisp: fix names in evaluator with the new ast

--- lisp/src/Evaluator.hs
+++ lisp/src/Evaluator.hs
@@ -2,7 +2,7 @@
 
 -- | Evaluator for the Lisp interpreter.
 -- This module implements the evaluation logic for AST nodes,
--- handling variable lookups, function calls, conditionals, and built-in operations.
+-- handling VariableRef lookups, function calls, conditionals, and built-in operations.
 module Evaluator
   ( eval,
     evalWithEnv,
@@ -11,61 +11,39 @@ module Evaluator
 where
 
 import Ast
-  ( Ast (..),
-    Environment,
-    EvalResult,
-    Evaluator,
-    ParamName,
-    Value (..),
-    VarName,
-    builtinDiv,
-    builtinEq,
-    builtinLt,
-    builtinMinus,
-    builtinMod,
-    builtinMult,
-    builtinPlus,
-    emptyEnv,
-    extendEnv,
-    isBuiltin,
-    lookupEnv,
-    paramToVar,
-    runEvaluator,
-    throwEvalError,
-    unVarName,
-  )
-import Control.Monad.State (get, gets, put)
+import Control.Monad.Except (ExceptT, throwError)
+import Control.Monad.State (State, get, gets, put)
 
 initialEnv :: Environment
 initialEnv = emptyEnv
 
-evalWithEnv :: Ast -> Environment -> EvalResult Value
+evalWithEnv :: Ast -> Environment -> EvalResult
 evalWithEnv ast = runEvaluator (eval ast)
 
-eval :: Ast -> Evaluator Value
+eval :: Ast -> Evaluator
 eval = \case
   LiteralInt n -> return $ VInt n
   LiteralBool b -> return $ VBool b
-  Variable name -> evalVariable name
+  VariableRef name -> evalVariable name
   Define name expr -> evalDefine name expr
   Lambda params body -> evalLambda params body
   Call func args -> evalCall func args
   If cond thenExpr elseExpr -> evalIf cond thenExpr elseExpr
 
-evalVariable :: VarName -> Evaluator Value
+evalVariable :: VarName -> Evaluator
 evalVariable name = do
   env <- get
   case lookupEnv name env of
     Just val -> return val
-    Nothing -> throwEvalError $ "Undefined variable: " ++ unVarName name
+    Nothing -> throwEvalError $ "Undefined VariableRef: " ++ unVarName name
 
-evalDefine :: VarName -> Ast -> Evaluator Value
+evalDefine :: VarName -> Ast -> Evaluator
 evalDefine name expr = do
   env <- get
   case expr of
     Lambda params body -> do
       let recursiveEnv = extendEnv name recursiveValue env
-          recursiveValue = VFunction params body recursiveEnv
+          recursiveValue = VProcedure params body recursiveEnv
       put $ extendEnv name recursiveValue env
       return VUnit
     _ -> do
@@ -73,20 +51,20 @@ evalDefine name expr = do
       put $ extendEnv name value env
       return VUnit
 
-evalLambda :: [ParamName] -> Ast -> Evaluator Value
+evalLambda :: [ParamName] -> Ast -> Evaluator
 evalLambda params body = do
-  gets (VFunction params body)
+  gets (VProcedure params body)
 
-evalCall :: Ast -> [Ast] -> Evaluator Value
+evalCall :: Ast -> [Ast] -> Evaluator
 evalCall funcExpr argExprs = do
   case funcExpr of
-    Variable name | isBuiltin name -> do
+    VariableRef name | isBuiltin name -> do
       args <- mapM eval argExprs
       evalBuiltin name args
     _ -> do
       func <- eval funcExpr
       case func of
-        VFunction params body closureEnv -> do
+        VProcedure params body closureEnv -> do
           args <- mapM eval argExprs
           if length params /= length args
             then throwEvalError $ "Function expects " ++ show (length params) ++ " arguments, got " ++ show (length args)
@@ -99,7 +77,7 @@ evalCall funcExpr argExprs = do
               return result
         _ -> throwEvalError "Attempted to call a non-function value"
 
-evalIf :: Ast -> Ast -> Ast -> Evaluator Value
+evalIf :: Ast -> Ast -> Ast -> Evaluator
 evalIf condExpr thenExpr elseExpr = do
   cond <- eval condExpr
   case cond of
@@ -107,7 +85,7 @@ evalIf condExpr thenExpr elseExpr = do
     VBool False -> eval elseExpr
     _ -> throwEvalError "Condition in 'if' must evaluate to a boolean"
 
-evalBuiltin :: VarName -> [Value] -> Evaluator Value
+evalBuiltin :: VarName -> [RuntimeValue] -> Evaluator
 evalBuiltin name args
   | name == builtinPlus = evalArithmetic (+) args
   | name == builtinMinus = evalArithmetic (-) args
@@ -118,34 +96,36 @@ evalBuiltin name args
   | name == builtinLt = evalLessThan args
   | otherwise = throwEvalError $ "Unknown built-in function: " ++ unVarName name
 
-evalArithmetic :: (Integer -> Integer -> Integer) -> [Value] -> Evaluator Value
+evalArithmetic :: (Integer -> Integer -> Integer) -> [RuntimeValue] -> Evaluator
 evalArithmetic op args = do
-  let extractInts [] = return []
-      extractInts (VInt n : rest) = (n :) <$> extractInts rest
-      extractInts _ = throwEvalError "Arithmetic operation requires integer arguments"
   nums <- extractInts args
   case nums of
     [] -> throwEvalError "Arithmetic operation requires at least one argument"
     [x] -> return $ VInt x
     (x : xs) -> return $ VInt $ foldl op x xs
+  where
+    extractInts :: [RuntimeValue] -> ExceptT ErrorMsg (State Environment) [Integer]
+    extractInts [] = return []
+    extractInts (VInt n : rest) = (n :) <$> extractInts rest
+    extractInts _ = throwError $ mkError "Arithmetic operation requires integer arguments"
 
-evalDivision :: [Value] -> Evaluator Value
+evalDivision :: [RuntimeValue] -> Evaluator
 evalDivision [VInt a, VInt b]
   | b == 0 = throwEvalError "Division by zero"
   | otherwise = return $ VInt (a `div` b)
 evalDivision _ = throwEvalError "Division requires exactly two integer arguments"
 
-evalModulo :: [Value] -> Evaluator Value
+evalModulo :: [RuntimeValue] -> Evaluator
 evalModulo [VInt a, VInt b]
   | b == 0 = throwEvalError "Modulo by zero"
   | otherwise = return $ VInt (a `mod` b)
 evalModulo _ = throwEvalError "Modulo requires exactly two integer arguments"
 
-evalEquality :: [Value] -> Evaluator Value
+evalEquality :: [RuntimeValue] -> Evaluator
 evalEquality [VInt a, VInt b] = return $ VBool (a == b)
 evalEquality [VBool a, VBool b] = return $ VBool (a == b)
 evalEquality _ = throwEvalError "Equality comparison requires exactly two arguments of the same type"
 
-evalLessThan :: [Value] -> Evaluator Value
+evalLessThan :: [RuntimeValue] -> Evaluator
 evalLessThan [VInt a, VInt b] = return $ VBool (a < b)
 evalLessThan _ = throwEvalError "Less-than comparison requires exactly two integer arguments"
