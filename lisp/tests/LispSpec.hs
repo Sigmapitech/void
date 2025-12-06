d1f03e252dedd2ef0e1454a45c6c3007058e3e4a
Evan Mahe <evan.mahe@epitech.eu>
2025-12-06 12:31:21 +0100

    lisp: fix Lispspec file to add tests from rebase

--- lisp/tests/LispSpec.hs
+++ lisp/tests/LispSpec.hs
@@ -1,21 +1,18 @@
-{-
--- EPITECH PROJECT, 2025
--- GLaDOS
--- File description:
--- Lisp-Interpreter Specs
--}
-
 module Main (main) where
 
+import ComplexExamplesSpec (spec)
+import EvaluatorSpec (spec)
 import LispAstSpec (astModuleSpec)
 import LispBootstrapSpec (bootstrapModuleSpec)
-import SexprtoASTSpec
-import SpecParser
-import Test.Hspec
+import SexprtoASTSpec (spec)
+import SpecParser (parserSpec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
+  parserSpec
   bootstrapModuleSpec
   astModuleSpec
+  EvaluatorSpec.spec
+  ComplexExamplesSpec.spec
   SexprtoASTSpec.spec
-  parserSpec
